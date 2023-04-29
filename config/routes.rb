9e44ae533423374fd4f124e4bc3d25bee1d6923b
gudiao21 <lucianodantas53@gmail.com>
2023-04-29 09:11:31 -0300

    commit estacionamento_app.rb files

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,3 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get "/clientes" , to: "clientes#index"
-  get "/user" , to: "user#index"
-  get "/home", to: "home#welcome"
-  get '/vehicles/new', to: 'vehicles#new', as: 'new_vehicle'
-  
-  Rails.application.routes.draw do
-    resources :vehicles
-  end
-  
+  resources :vehicles
 end
