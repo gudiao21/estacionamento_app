9822a665231eda6d60defff9f38f8542cedcd048
gudiao21 <lucianodantas53@gmail.com>
2023-05-01 20:06:46 -0300

    Committing new.html.erb schema.rb views/vehicles and migrate files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'home#welcome'
   get '/vehicles/new', to: 'vehicles#new', as: 'new_vehicle'
+  #Em 'to: 'vehicles#new' 'new' é o nome do método(def) dentro de vehicls_controller.rb
+  #E 'vehicles é o nome do controller.
 end
