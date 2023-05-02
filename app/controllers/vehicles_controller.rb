3aa24dfe96c00f641c0e510876eae08ec158b846
gudiao21 <lucianodantas53@gmail.com>
2023-05-02 10:18:33 -0300

    Committing vehicles_controller.rb

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,5 +1,18 @@
 class VehiclesController < ApplicationController
-    def new
+    def new 
         @vehicle = ::Vehicle.new
     end
+
+    def create
+        @vehicle = Vehicle.new(vehicle_params)
+
+        if @vehicle.save
+            redirect_to wlcome_path, notice: 'Veículo cadastrado com sucesso!'
+        else
+            render :new
+
+            
+        else
+            
+        end
 end
