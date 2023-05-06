390818a3556db326dad29701d16ef51d27ab3842
gudiao21 <lucianodantas53@gmail.com>
2023-05-06 15:05:50 -0300

    Committing saidas_controller new.html.erb e search.html.erb

--- app/controllers/saidas_controller.rb
+++ app/controllers/saidas_controller.rb
@@ -5,8 +5,8 @@ class SaidasController < ApplicationController
     
     def create
         @vehicle = Vehicle.find_by(placa: params[:placa])
-        @vehicle.hora_saida = params[:hora_saida]
-        
+        @vehicle.hora_saida = params[:vehicle][:hora_saida]
+        @vehicle.total_a_pagar_por_veiculo = calculo(@vehicle.placa, )
         if @vehicle.save
             redirect_to welcome_path, notice: 'Saída do veículo cadastrada com sucesso!'
         else
