541e8fbb4342a319b4ac4984bd48415e5f133f56
gudiao21 <lucianodantas53@gmail.com>
2023-05-06 18:14:31 -0300

    Committing saidas_controller new.html.erb

--- app/controllers/saidas_controller.rb
+++ app/controllers/saidas_controller.rb
@@ -6,11 +6,21 @@ class SaidasController < ApplicationController
     def create
         @vehicle = Vehicle.find_by(placa: params[:placa])
         @vehicle.hora_saida = params[:vehicle][:hora_saida]
-        @vehicle.total_a_pagar_por_veiculo = calculo(@vehicle.placa, )
+        @vehicle.total_a_pagar_por_veiculo = calculo(@vehicle.placa, @vehicle.hora_entrada, @vehicle.hora_saida)
+        
         if @vehicle.save
             redirect_to welcome_path, notice: 'Saída do veículo cadastrada com sucesso!'
         else
             render :new
         end
     end
+
+    private
+
+    def calculo(placa, hora_entrada, hora_saida)
+        minuto_total = ((hora_saida) - (hora_entrada)) / 60
+        resultado = minuto_total * 0.17
+        return resultado
+    end
+
 end
