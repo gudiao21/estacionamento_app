87188f481f95e95a17381bfb6c3ad69202ddfef2
gudiao21 <lucianodantas53@gmail.com>
2023-05-11 08:46:54 -0300

    Committing saidas_controller, new.html.erb  e routes.rb

--- app/controllers/saidas_controller.rb
+++ app/controllers/saidas_controller.rb
@@ -5,23 +5,32 @@ class SaidasController < ApplicationController
     
     def create
         @vehicle = Vehicle.find_by(placa: params[:placa])
-        @vehicle.hora_saida = params[:vehicle][:hora_saida]
-        @vehicle.total_a_pagar_por_veiculo = calculo(@vehicle.placa, @vehicle.hora_entrada, @vehicle.hora_saida)
         #debugger
-        if @vehicle.save
-            flash[:notice] = 'Saída do veículo cadastrada com sucesso!'
-            flash[:vehicle_attributes] = @vehicle.attributes.slice('placa', 'nome_veiculo', 'dono_do_veiculo', 'hora_entrada', 'hora_saida', 'total_a_pagar_por_veiculo')
-            redirect_to welcome_path
+        
+        if @vehicle
+            @vehicle.hora_saida = params[:vehicle][:hora_saida]
+            @vehicle.total_a_pagar_por_veiculo = calculo(@vehicle.placa, @vehicle.hora_entrada, @vehicle.hora_saida)
+            #debugger
+            
+            if @vehicle.save
+                flash[:notice] = 'Saída do veículo cadastrada com sucesso!'
+                flash[:vehicle_attributes] = @vehicle.attributes.slice('placa', 'nome_veiculo', 'dono_do_veiculo', 'hora_entrada', 'hora_saida', 'total_a_pagar_por_veiculo')
+                #redirect_to welcome_path
+            else
+                flash[:error] = 'Erro ao cadastrar saída do veículo.'
+                render :new
+        
+            end
         else
-            flash[:error] = 'Erro ao cadastrar saída do veículo.'
-            render :new
-        end
+            flash[:error] = 'Veículo não encontrado.'
+            render :new 
+        end       
     end
 
     private
 
     def calculo(placa, hora_entrada, hora_saida)
-        segundos_total = hora_saida - hora_entrada).to_i
+        segundos_total = (hora_saida - hora_entrada).to_i
         minutos_total = (segundos_total) / 60
         resultado = minutos_total * 0.17
         return resultado
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'home#welcome', as: 'welcome'
+  get '/welcome', to: 'home#welcome', as: 'welcome'
   get '/vehicles/new', to: 'vehicles#new', as: 'new_vehicle'
   #Em 'to: 'vehicles#new' 'new' é o nome do método(def) dentro de vehicls_controller.rb
   #E 'vehicles' é o nome do controller.
   post '/vehicles', to: 'vehicles#create', as: 'vehicles'
-  get '/saidas/new', to: 'saidas#new', as: 'new_saida' #Chama o método 'new'do controlador 'SaidasController'.
+  get '/saidas/new', to: 'saidas#new', as: 'new_saida' #Chama o método 'new'do controlador 'saidas'.
   post '/saidas/new', to: 'saidas#create', as:'create_saida'
   get '/busca', to: 'vehicles#search', as: 'search_vehicle'
   
